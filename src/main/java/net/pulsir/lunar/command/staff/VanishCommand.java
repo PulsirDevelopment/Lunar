7311050ce0b192838e6d671c45161b949c586422
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2024-06-27 02:13:45 -0400

    Simplify vanish command method and clean code.

--- src/main/java/net/pulsir/lunar/command/staff/VanishCommand.java
+++ src/main/java/net/pulsir/lunar/command/staff/VanishCommand.java
@@ -7,17 +7,19 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
 
 public class VanishCommand implements CommandExecutor {
 
     @Override
     @Deprecated
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-
         if (!(sender instanceof Player player)) return false;
         if (!(player.hasPermission("lunar.staff"))) {
             sender.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
@@ -26,44 +28,57 @@ public class VanishCommand implements CommandExecutor {
         }
 
         if (Lunar.getInstance().getData().getVanish().contains(player.getUniqueId())) {
-            Lunar.getInstance().getData().getVanish().remove(player.getUniqueId());
-            for (Player onlinePlayers : Bukkit.getOnlinePlayers()) {
-                onlinePlayers.showPlayer(player);
-            }
-            
-            Lunar.getInstance().getData().getOnlinePlayers().add(player.getUniqueId());
-            for (final String vanishEffect : Lunar.getInstance().getConfiguration().getConfiguration().getStringList("vanish-effects")) {
-                player.removePotionEffect(Objects.requireNonNull(PotionEffectType.getByName(vanishEffect)));
-            }
-            player.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
-                    .getConfiguration().getString("VANISH.DISABLED")));
+            disableVanish(player);
         } else {
-            Lunar.getInstance().getData().getVanish().add(player.getUniqueId());
-            for (Player onlinePlayers : Bukkit.getOnlinePlayers()) {
-                if (!onlinePlayers.hasPermission("lunar.staff")) {
-                    onlinePlayers.hidePlayer(player);
-                }
-            }
-
-            Lunar.getInstance().getData().getOnlinePlayers().remove(player.getUniqueId());
-            for (final String vanishEffect : Lunar.getInstance().getConfiguration().getConfiguration().getStringList("vanish-effects")) {
-                player.addPotionEffect(Objects.requireNonNull(PotionEffectType.getByName(vanishEffect)).createEffect(999999, 0));
-            }
-            player.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
-                    .getConfiguration().getString("VANISH.ENABLED")));
-                    
+            enableVanish(player);
         }
 
-        if (Lunar.getInstance().getData().getStaffMode().contains(player.getUniqueId())) {
-            if (Lunar.getInstance().getData().getVanish().contains(player.getUniqueId())) {
-                player.getInventory().setItem(Lunar.getInstance().getConfiguration().getConfiguration()
-                        .getInt("staff-items.vanish.slot"), Staff.unvanish());
-            } else {
-                player.getInventory().setItem(Lunar.getInstance().getConfiguration().getConfiguration()
-                        .getInt("staff-items.vanish.slot"), Staff.vanish());
-            }
-        }
-
+        updateVanishItem(player);
         return true;
+    }
+
+    private void disableVanish(Player player) {
+        Lunar.getInstance().getData().getVanish().remove(player.getUniqueId());
+        Lunar.getInstance().getData().getOnlinePlayers().add(player.getUniqueId());
+
+        Bukkit.getOnlinePlayers().forEach(online -> online.showPlayer(Lunar.getInstance(), player));
+        player.getActivePotionEffects().forEach(effect -> player.removePotionEffect(effect.getType()));
+
+        player.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
+                .getConfiguration().getString("VANISH.DISABLED")));
+    }
+
+    @Deprecated
+    private void enableVanish(Player player) {
+        Set<UUID> vanishSet = Lunar.getInstance().getData().getVanish();
+        vanishSet.add(player.getUniqueId());
+        Lunar.getInstance().getData().getOnlinePlayers().remove(player.getUniqueId());
+
+        Bukkit.getOnlinePlayers()
+                .stream()
+                .filter(online -> !vanishSet.contains(online.getUniqueId()))
+                .forEach(online -> online.hidePlayer(Lunar.getInstance(), player));
+
+        for (final String vanishEffect : Lunar.getInstance().getConfiguration().getConfiguration().getStringList("vanish-effects")) {
+            player.addPotionEffect(new PotionEffect(Objects.requireNonNull(PotionEffectType.getByName(vanishEffect)),
+                    999999,
+                    0)
+                    .withParticles(false));
+        }
+        player.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
+                .getConfiguration().getString("VANISH.ENABLED")));
+    }
+
+    private void updateVanishItem(Player player) {
+        // If player is not in staff mode don't update item.
+        if (!Lunar.getInstance().getData().getStaffMode().contains(player.getUniqueId())) return;
+
+        if (Lunar.getInstance().getData().getVanish().contains(player.getUniqueId())) {
+            player.getInventory().setItem(Lunar.getInstance().getConfiguration().getConfiguration()
+                    .getInt("staff-items.vanish.slot"), Staff.unvanish());
+        } else {
+            player.getInventory().setItem(Lunar.getInstance().getConfiguration().getConfiguration()
+                    .getInt("staff-items.vanish.slot"), Staff.vanish());
+        }
     }
 }
