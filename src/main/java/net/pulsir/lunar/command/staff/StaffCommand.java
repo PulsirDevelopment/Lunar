517e3cd0a594d9d48feb1d81d9958b1d72ea32fc
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2024-06-27 02:14:41 -0400

    Simplify staff command method and clean code. I also removed staffNameTags list since they seems like does nothing.

--- src/main/java/net/pulsir/lunar/command/staff/StaffCommand.java
+++ src/main/java/net/pulsir/lunar/command/staff/StaffCommand.java
@@ -1,19 +1,20 @@
 package net.pulsir.lunar.command.staff;
 
-import net.kyori.adventure.text.Component;
 import net.pulsir.lunar.Lunar;
 import net.pulsir.lunar.utils.staff.Staff;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
 
 public class StaffCommand implements CommandExecutor {
 
@@ -26,54 +27,59 @@ public class StaffCommand implements CommandExecutor {
             return false;
         }
 
-        if (Lunar.getInstance().getData().getStaffMode().contains(player.getUniqueId())) {
-            Lunar.getInstance().getData().getStaffMode().remove(player.getUniqueId());
-
-            player.setGameMode(GameMode.SURVIVAL);
-            player.getInventory().setContents(Lunar.getInstance().getData().getInventories().get(player.getUniqueId()));
-            Lunar.getInstance().getData().getInventories().remove(player.getUniqueId());
-
-            player.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
-                    .getConfiguration().getString("STAFF-MODE.DISABLED")));
-
-            for (final String staffEffect : Lunar.getInstance().getConfiguration().getConfiguration().getStringList("staff-effects")) {
-                player.removePotionEffect(Objects.requireNonNull(PotionEffectType.getByName(staffEffect)));
-            }
-
+        Set<UUID> staffSet = Lunar.getInstance().getData().getStaffMode();
+        if (!staffSet.contains(player.getUniqueId())) {
+            enableStaffMode(player);
         } else {
-            Lunar.getInstance().getData().getStaffMode().add(player.getUniqueId());
-            if (Lunar.getInstance().getConfiguration().getConfiguration().getBoolean("force-vanish")) {
-                Lunar.getInstance().getData().getVanish().add(player.getUniqueId());
-            }
-
-            Lunar.getInstance().getData().getInventories().put(player.getUniqueId(), player.getInventory().getContents());
-            player.getInventory().clear();
-            player.setGameMode(GameMode.CREATIVE);
-            Staff.items(player);
-
-            player.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
-                    .getConfiguration().getString("STAFF-MODE.ENABLED")));
-
-            if (Lunar.getInstance().getConfiguration().getConfiguration().getBoolean("lunar-api")) {
-                Component staffComponent =
-                        Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getConfiguration().getConfiguration().getString("LUNAR-CLIENT.STAFF"));
-
-                List<Component> staffNameTag = new ArrayList<>();
-                for (final String lines : Lunar.getInstance().getConfiguration().getConfiguration().getStringList("lunar-client.staff-nametag")) {
-                    staffNameTag.add(Lunar.getInstance().getMessage().getMessage(lines.replace("{player}", player.getName())));
-                }
-            }
-
-            for (final String staffEffect : Lunar.getInstance().getConfiguration().getConfiguration().getStringList("staff-effects")) {
-                player.addPotionEffect(Objects.requireNonNull(PotionEffectType.getByName(staffEffect)).createEffect(999999, 0));
-            }
-
-            List<Component> staffNameTag = new ArrayList<>();
-            for (final String lines : Lunar.getInstance().getConfiguration().getConfiguration().getStringList("lunar-client.staff-nametag")) {
-                staffNameTag.add(Lunar.getInstance().getMessage().getMessage(lines.replace("{player}", player.getName())));
-            }
+            disableStaffMode(player);
         }
 
         return true;
+    }
+
+    private void disableStaffMode(Player player) {
+        Lunar.getInstance().getData().getStaffMode().remove(player.getUniqueId());
+
+        player.setCollidable(true);
+        player.setGameMode(GameMode.SURVIVAL);
+        Bukkit.getOnlinePlayers().forEach(online -> online.showPlayer(Lunar.getInstance(), player));
+        player.getActivePotionEffects().forEach(effect -> player.removePotionEffect(effect.getType()));
+
+        player.getInventory().setContents(Lunar.getInstance().getData().getInventories().get(player.getUniqueId()));
+        Lunar.getInstance().getData().getInventories().remove(player.getUniqueId());
+
+        player.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
+                .getConfiguration().getString("STAFF-MODE.DISABLED")));
+    }
+
+    @Deprecated
+    private void enableStaffMode(Player player) {
+        Lunar.getInstance().getData().getStaffMode().add(player.getUniqueId());
+
+        if (Lunar.getInstance().getConfiguration().getConfiguration().getBoolean("force-vanish")) {
+            Set<UUID> vanishSet = Lunar.getInstance().getData().getVanish();
+            vanishSet.add(player.getUniqueId());
+
+            Bukkit.getOnlinePlayers()
+                    .stream()
+                    .filter(online -> !vanishSet.contains(online.getUniqueId()))
+                    .forEach(online -> online.hidePlayer(Lunar.getInstance(), player));
+        }
+
+        Lunar.getInstance().getData().getInventories().put(player.getUniqueId(), player.getInventory().getContents());
+        player.getInventory().clear();
+        player.setCollidable(false);
+        player.setGameMode(GameMode.CREATIVE);
+        Staff.items(player);
+
+        for (final String staffEffect : Lunar.getInstance().getConfiguration().getConfiguration().getStringList("staff-effects")) {
+            player.addPotionEffect(new PotionEffect(Objects.requireNonNull(PotionEffectType.getByName(staffEffect)),
+                    999999,
+                    0)
+                    .withParticles(false));
+        }
+
+        player.sendMessage(Lunar.getInstance().getMessage().getMessage(Lunar.getInstance().getLanguage()
+                .getConfiguration().getString("STAFF-MODE.ENABLED")));
     }
 }
