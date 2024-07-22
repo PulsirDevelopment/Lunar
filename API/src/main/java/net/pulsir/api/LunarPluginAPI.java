2db36a6b71e870cb3c92f0bc6008489f1f654e0c
KacperM <vkacperpl@gmail.com>
2024-07-22 15:41:21 +0200

    maven-publish

--- API/src/main/java/net/pulsir/api/LunarPluginAPI.java
+++ API/src/main/java/net/pulsir/api/LunarPluginAPI.java
@@ -4,7 +4,6 @@ import net.pulsir.api.chat.ChatManager;
 import net.pulsir.api.inventory.InventoryManager;
 import net.pulsir.api.session.SessionManager;
 import net.pulsir.api.staff.StaffManager;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public interface LunarPluginAPI extends Plugin {
